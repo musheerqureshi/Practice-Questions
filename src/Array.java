ed3a654cb3ec2caf79a5e4cc04d66a4b47c1f939
musheerqureshi <musheeralam794@gmail.com>
2024-09-21 19:50:59 +0530

    "This is practice question of Array"

--- src/Array.java
+++ src/Array.java
@@ -6,8 +6,12 @@ public class Array {
         marks[1] = 84;
         marks[2] = 95;
 
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+       // System.out.println(marks[0]);
+       // System.out.println(marks[1]);
+       // System.out.println(marks[2]);
+
+        for (int i= 0; i < 3; i++){
+            System.out.println(marks[i]);
+        }
     }
 }
