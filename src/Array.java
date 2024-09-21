cb04961c531892107e12f965f92babc7a1f30abe
musheerqureshi <musheeralam794@gmail.com>
2024-09-21 19:59:53 +0530

    "This is practice question of Array"

--- src/Array.java
+++ src/Array.java
@@ -1,10 +1,14 @@
 public class Array {
     public static void main(String[] args) {
-        int[] marks = new int[3];
 
-        marks[0] = 89;
-        marks[1] = 84;
-        marks[2] = 95;
+        int marks[] = {89, 84, 95};
+        //int[] marks = new int[3];
+
+       // marks[0] = 89;
+       // marks[1] = 84;
+      //  marks[2] = 95;
+
+
 
        // System.out.println(marks[0]);
        // System.out.println(marks[1]);
