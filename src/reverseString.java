f393bf1c9b9594a980327c3f06e79bdb143d62d3
musheerqureshi <musheeralam794@gmail.com>
2024-09-24 17:56:16 +0530

    "This is practice question"

---
+++ src/reverseString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class reverseString {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello");
+        System.out.println(sb);
+
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
